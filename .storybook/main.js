b58714116c1bb1211ff45ece838e9b806fa3fe74
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-03-14 19:11:21 +0400

    Storybook location refactoring

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,9 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    '../pages/**/*.stories.mdx',
+    '../pages/**/*.stories.@(js|jsx|ts|tsx)',
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   "addons": [
     "@storybook/addon-links",
---
+++ pages/Home.stories.tsx
@@ -0,0 +1,8 @@
+import Home from "./index";
+
+export default {
+  title: "Pages/Home",
+  component: Home,
+};
+
+export const HomePage = () => <Home />;
