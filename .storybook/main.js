9528494686cd171451bbbe685016e8ada465cfca
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-03-16 15:28:20 +0400

    Add aliases to Typescript and Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   "stories": [
     '../pages/**/*.stories.mdx',
@@ -10,6 +12,15 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
+  "webpackFinal": async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
+  },
   "framework": "@storybook/react",
   "core": {
     "builder": "webpack5"
