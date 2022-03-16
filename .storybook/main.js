66a0351dc9641b02176aa739d0396c05dcb50284
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-03-16 20:04:35 +0400

    Reformat configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,19 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
-  "stories": [
-    '../pages/**/*.stories.mdx',
-    '../pages/**/*.stories.@(js|jsx|ts|tsx)',
-    '../components/**/*.stories.mdx',
-    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+  stories: [
+    "../pages/**/*.stories.mdx",
+    "../pages/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "staticDirs": ["../public"],
-  "addons": [
+  staticDirs: ["../public"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "webpackFinal": async (config) => {
+  webpackFinal: async (config) => {
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
       new TsconfigPathsPlugin({
@@ -22,12 +22,12 @@ module.exports = {
     ];
     return config;
   },
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
   },
   // https://github.com/storybookjs/storybook/issues/10231
-  "features": {
-    "emotionAlias": false,
+  features: {
+    emotionAlias: false,
   },
-}
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,8 @@
 import * as NextImage from "next/image";
-import { ThemeProvider, Global } from '@emotion/react';
+import { ThemeProvider, Global } from "@emotion/react";
 
-import { Themes } from '../styles/themes';
-import { GlobalStyles } from '../styles/global';
+import { Themes } from "../styles/themes";
+import { GlobalStyles } from "../styles/global";
 
 const OriginalNextImage = NextImage.default;
 
@@ -12,8 +12,11 @@ Object.defineProperty(NextImage, "default", {
 });
 
 const withThemeProvider = (Story, context) => {
-  const background = context.globals.backgrounds?.value || parameters.backgrounds.defaultColor;
-  const theme = Object.values(Themes).find(theme => theme.background === background)
+  const background =
+    context.globals.backgrounds?.value || parameters.backgrounds.defaultColor;
+  const theme = Object.values(Themes).find(
+    (theme) => theme.background === background
+  );
 
   return (
     <ThemeProvider theme={theme}>
@@ -27,17 +30,17 @@ const withGlobalStyles = (Story, context) => (
     <Global styles={GlobalStyles} />
     <Story {...context} />
   </>
-)
+);
 
 export const decorators = [withThemeProvider, withGlobalStyles];
 
 export const parameters = {
   backgrounds: {
-    default: 'dark',
-    defaultColor: '#5e5c64',
+    default: "dark",
+    defaultColor: "#5e5c64",
     values: [
-      { name: 'dark', value: '#5e5c64' },
-      { name: 'light', value: '#e4ebf5' },
+      { name: "dark", value: "#5e5c64" },
+      { name: "light", value: "#e4ebf5" },
     ],
   },
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -47,4 +50,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
