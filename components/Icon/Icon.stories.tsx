e172cdf854c86d8283c70a5b9629797228aa7ed1
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-03-18 20:05:35 +0400

    Add Icon component

---
+++ components/Icon/Icon.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Icon } from "./index";
+
+export default {
+  title: "Content/Icon",
+  component: Icon,
+} as ComponentMeta<typeof Icon>;
+
+const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
+
+export const BasicIcon = Template.bind({});
+BasicIcon.args = {
+  name: "Home",
+};
