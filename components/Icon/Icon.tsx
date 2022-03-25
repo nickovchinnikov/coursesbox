d24f79fa6529fbddded060b5326f01aa58218e87
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-03-25 16:57:23 +0400

    Icon component refactoring

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -1,43 +1,25 @@
 import { FC } from "react";
 import styled from "@emotion/styled";
-import { css } from "@emotion/react";
 
 import { Icons } from "./Icons";
 
 export type AvailableIcons = keyof typeof Icons;
 
-type WrapperProps = {
-  /** Width and height */
-  size?: number;
-};
-
 export type Props = {
   /** Icon name */
   name: AvailableIcons;
-} & WrapperProps &
-  React.SVGProps<SVGSVGElement>;
-
-const Wrapper = styled.div<WrapperProps>`
-  color: ${({ theme }) => theme.font.regular};
-  ${({ size }) => {
-    const sizeInRem = `${size}rem`;
-    return css`
-      width: ${sizeInRem};
-      height: ${sizeInRem};
-    `;
-  }}
-`;
+  /** Width and height */
+  size?: number;
+} & React.SVGProps<SVGSVGElement>;
 
 // https://reactsvgicons.com/search
 
 export const Icon: FC<Props> = ({ name, size = 2, ...rest }) => {
-  const Icon = Icons[name];
+  const Icon = styled(Icons[name])`
+    color: ${({ theme }) => theme.font.regular};
+  `;
   const sizeInRem = `${size}rem`;
   const sizes = { width: sizeInRem, height: sizeInRem };
 
-  return (
-    <Wrapper size={size}>
-      <Icon {...sizes} {...rest} />
-    </Wrapper>
-  );
+  return <Icon role="img" aria-label={name} {...sizes} {...rest} />;
 };
