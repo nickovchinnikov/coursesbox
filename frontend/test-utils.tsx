9b2f3f3ec57d5708ace80bf84c92854de5030c25
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-04-27 11:12:06 +0400

    Add to the test-utils Wrapper Redux-Provider

--- frontend/test-utils.tsx
+++ frontend/test-utils.tsx
@@ -1,11 +1,15 @@
 import { FC, ReactElement } from "react";
+import { Provider } from "react-redux";
 import { render, RenderOptions } from "@testing-library/react";
 import { ThemeProvider } from "@emotion/react";
 
 import { Themes } from "./styles/themes";
+import { store } from "./store";
 
 const Wrapper: FC = ({ children }) => (
-  <ThemeProvider theme={Themes.light}>{children}</ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={Themes.light}>{children}</ThemeProvider>
+  </Provider>
 );
 
 const customRender = (ui: ReactElement, options?: RenderOptions) =>
