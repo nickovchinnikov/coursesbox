736757a3b672e758df7bf7ee52e7d5a98137fb74
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-05-02 20:01:40 +0400

    Add default theme background and color

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,6 +4,20 @@ import { Global } from "@emotion/react";
 import { GlobalStyles } from "@/styles/global";
 
 export default function Document() {
+  const setInitialTheme = `
+    var isDarkInStorage = Boolean(localStorage.getItem("theme") === "dark");
+
+    var isDark =
+      window.matchMedia("prefers-color-scheme: dark").matches || isDarkInStorage;
+
+    if (isDark) {
+      document.documentElement.style.setProperty("--themeBackgroundColor", "#5e5c64");
+      document.documentElement.style.setProperty("--themeColor", "#E4EBF5e6");
+    } else {
+      document.documentElement.style.setProperty("--themeBackgroundColor", "#e4ebf5");
+      document.documentElement.style.setProperty("--themeColor", "#504e55e6");
+    }
+  `;
   return (
     <Html>
       <Head>
@@ -11,6 +25,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Poppins|Monoton&display=swap"
         />
+        <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
         <Global styles={GlobalStyles} />
       </Head>
       <body>
