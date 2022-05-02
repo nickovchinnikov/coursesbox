096f0f8f3f692f87d14301062cf50dca9ce1fb7c
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-05-02 20:02:28 +0400

    Move theme functionality to the Layout

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -1,5 +1,8 @@
-import { FC } from "react";
+import { useState, useLayoutEffect, useEffect, FC } from "react";
 import Link from "next/link";
+import { ThemeProvider } from "@emotion/react";
+
+import { Themes } from "@/styles/themes";
 
 import { IconButton } from "@/components/IconButton";
 import { StyledLink } from "@/components/StyledLink";
@@ -14,38 +17,57 @@ import {
   Footer,
 } from "./components";
 
-type Props = {
-  isDark: boolean;
-  onThemeToggle: () => void;
-};
+const useIsomorphicLayoutEffect =
+  typeof window !== "undefined" ? useLayoutEffect : useEffect;
 
-export const Layout: FC<Props> = ({ children, isDark, onThemeToggle }) => (
-  <Wrapper>
-    <Link href="/" passHref>
-      <LogoLink>
-        <StyledLogo size={3}>
-          <span className="logo_short">C8X</span>
-          <span className="logo_full">CoursesBox</span>
-        </StyledLogo>
-      </LogoLink>
-    </Link>
-    <MainNav>
-      <Link href="/all" passHref>
-        <StyledLink>All</StyledLink>
-      </Link>
-      <Link href="/login" passHref>
-        <IconButton name="Login" size={1} />
-      </Link>
-      <IconButton
-        name={isDark ? "Moon" : "Sun"}
-        size={1}
-        onClick={onThemeToggle}
-      />
-    </MainNav>
-    <SearchInput icon="Search" placeholder="Search" onChange={() => null} />
-    <Content>{children}</Content>
-    <Footer>
-      © {new Date().getFullYear()} NickOvchinnikov. All rights reserved.
-    </Footer>
-  </Wrapper>
-);
+export const Layout: FC = ({ children }) => {
+  const [isDark, setIsDark] = useState(true);
+
+  const toggleDark = () => {
+    localStorage.setItem("theme", isDark ? "light" : "dark");
+    setIsDark(!isDark);
+  };
+
+  useIsomorphicLayoutEffect(() => {
+    const isDark =
+      Boolean(localStorage.getItem("theme") === "dark") ||
+      window.matchMedia("prefers-color-scheme: dark").matches;
+
+    setIsDark(isDark);
+  }, []);
+
+  const theme = Themes[isDark ? "dark" : "light"];
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <Link href="/" passHref>
+          <LogoLink>
+            <StyledLogo size={3}>
+              <span className="logo_short">C8X</span>
+              <span className="logo_full">CoursesBox</span>
+            </StyledLogo>
+          </LogoLink>
+        </Link>
+        <MainNav>
+          <Link href="/all" passHref>
+            <StyledLink>All</StyledLink>
+          </Link>
+          <Link href="/login" passHref>
+            <IconButton name="Login" size={1} />
+          </Link>
+          <IconButton
+            name={isDark ? "Moon" : "Sun"}
+            size={1}
+            onClick={toggleDark}
+          />
+        </MainNav>
+        <SearchInput icon="Search" placeholder="Search" onChange={() => null} />
+        <Content>{children}</Content>
+        <Footer>
+          © {new Date().getFullYear()} NickOvchinnikov. All rights reserved.
+        </Footer>
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,35 +1,12 @@
-import { useState, useEffect } from "react";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "@emotion/react";
-
-import { Themes } from "@/styles/themes";
 
 import { Layout } from "@/components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isDark, setIsDark] = useState(false);
-
-  const toggleDark = () => {
-    localStorage.setItem("theme", isDark ? "light" : "dark");
-    setIsDark(!isDark);
-  };
-
-  useEffect(() => {
-    const isDark = Boolean(localStorage.getItem("theme") === "dark");
-
-    setIsDark(
-      window.matchMedia("prefers-color-scheme: dark").matches || isDark
-    );
-  }, []);
-
-  const theme = Themes[isDark ? "dark" : "light"];
-
   return (
-    <ThemeProvider theme={theme}>
-      <Layout isDark={isDark} onThemeToggle={toggleDark}>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
