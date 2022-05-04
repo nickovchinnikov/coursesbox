6ffe8988559e4681e12bd33041e97b24bfc3635f
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-05-04 12:08:16 +0400

    Setup jest for msw

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -5,6 +5,8 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 
+import { server } from "./mocks/server";
+
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
@@ -18,3 +20,17 @@ Object.defineProperty(window, "matchMedia", {
     dispatchEvent: jest.fn(),
   })),
 });
+
+// Establish API mocking before all tests.
+
+beforeAll(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+
+// so they don't affect other tests.
+
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished.
+
+afterAll(() => server.close());
---
+++ frontend/mocks/handlers.ts
@@ -0,0 +1,39 @@
+import { rest } from "msw";
+
+import { LoginData } from "@/services/userSlice";
+
+import { mockUser } from "./user";
+
+const api_url = process.env.NEXT_PUBLIC_STRAPI_API_URL;
+
+const ValidationError = {
+  error: {
+    status: 400,
+    name: "ValidationError",
+    message: "Invalid identifier or password",
+    details: {},
+  },
+};
+
+export const handlers = [
+  rest.post<LoginData>(`${api_url}/auth/local`, (req, res, ctx) => {
+    const data = req.body;
+
+    if (
+      data.identifier === mockUser.user.email &&
+      data.password === mockUser.user.password
+    ) {
+      return res(ctx.status(200), ctx.json(mockUser));
+    }
+
+    return res(ctx.status(400), ctx.json(ValidationError));
+  }),
+
+  rest.get(`${api_url}/users/me`, (req, res, ctx) => {
+    if (req.headers.get("Authorization") === `Bearer ${mockUser.jwt}`) {
+      return res(ctx.status(200), ctx.json(mockUser.user));
+    }
+
+    return res(ctx.status(400), ctx.json(ValidationError));
+  }),
+];
