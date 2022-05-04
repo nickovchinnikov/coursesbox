2c1c7338366024b1814664b8b9517806ae67203b
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-05-04 18:42:28 +0400

    Add redux provider to the stories

--- frontend/stories/User.stories.tsx
+++ frontend/stories/User.stories.tsx
@@ -1,8 +1,28 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import { mockUser } from "@/mocks/user";
 import User from "@/pages/user";
+import { rootReducer } from "@/store";
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: {
+    user: {
+      jwt: mockUser.jwt,
+      username: mockUser.user.username,
+      email: mockUser.user.email,
+    },
+  },
+});
 
 export default {
   title: "Pages/User",
   component: User,
 };
 
-export const UserPage = () => <User />;
+export const UserPage = () => (
+  <Provider store={store}>
+    <User />
+  </Provider>
+);
