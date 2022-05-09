741ffdaa1741bb2d830697eb6f4c173e1b3ea5b3
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-05-09 14:07:26 +0400

    Add data types

---
+++ frontend/types.ts
@@ -0,0 +1,57 @@
+export type Data<A = Record<string, unknown>> = {
+  id: number;
+  attributes: A;
+};
+
+export type ImageFormats = {
+  name: string;
+  url: string;
+  width: number;
+  height: number;
+};
+
+export type Image = Data<
+  ImageFormats & {
+    formats: {
+      thumbnail: ImageFormats;
+      large: ImageFormats;
+      medium: ImageFormats;
+      small: ImageFormats;
+    };
+  }
+>;
+
+export type Course = Data<{
+  header: string;
+  url: string;
+  description: string;
+  publishedAt: string;
+  cover: {
+    data: Image;
+  };
+  images: {
+    data: Image[];
+  };
+}>;
+
+export type Error = {
+  status: number;
+  name: string;
+  message: string;
+  details: any;
+};
+
+export type Meta = {
+  pagination?: {
+    page: number;
+    pageSize: number;
+    pageCount: number;
+    total: number;
+  };
+};
+
+export type Response<D> = {
+  data: D;
+  error?: Error;
+  meta: Meta;
+};
