f034964aed204e37d5a7827c9e3ea46860682a44
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-05-10 16:59:13 +0400

    Add to the index page content

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
 import styled from "@emotion/styled";
 
+import { Course as CourseType, Response } from "@/types";
 import { Course } from "@/components/Course";
 
 const CoursesWrapper = styled.div`
@@ -11,46 +12,86 @@ const CoursesWrapper = styled.div`
   margin: 2vh 1vw;
 `;
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>CoursesBox</title>
-        <meta name="description" content="IT courses for everyone" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <CoursesWrapper>
-        {Array(4)
-          .fill("")
-          .map(() => (
-            <Course
-              key={Math.random()}
-              header="Hands-On React. Build advanced React JS Frontend with expert"
-              link="/hands-on-reactjs"
-              imageProps={{
-                width: 1368,
-                height: 770,
-                alt: "Logo for Hands-On React",
-                src: "/covers/hands-on_reactjs_cover.png",
-              }}
-            >
-              <>
-                React is the most popular library for building frontend web
-                applications. Step-by-step by diving into all the basics,
-                I&apos;ll introduce you to advanced concepts as well. We&apos;ll
-                build the minesweeper application from scratch We&apos;ll build
-                the minesweeper application from scratch:
-                <ul>
-                  <li>setup of the development environment</li>
-                  <li>configuration of the React JS app</li>
-                  <li>basic algorithms of Minesweeper</li>
-                </ul>
-              </>
-            </Course>
-          ))}
-      </CoursesWrapper>
-    </>
-  );
+type CoursesResponce = Response<CourseType[]>;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const api_url = process.env.NEXT_PUBLIC_STRAPI_API_URL;
+
+  const responce = await fetch(`${api_url}/courses?populate=*`, {
+    method: "GET",
+  });
+
+  const { data: courses, meta, error }: CoursesResponce = await responce.json();
+
+  const status = error?.status;
+
+  if (status && (status < 200 || status >= 300)) {
+    return {
+      props: {
+        courses: [],
+        meta: {},
+      },
+    };
+  }
+
+  return {
+    props: {
+      courses,
+      meta,
+    },
+  };
 };
+
+const strapi_url = process.env.NEXT_PUBLIC_STRAPI_URL;
+
+const Home: NextPage<{
+  courses: CourseType[];
+}> = ({ courses }) => (
+  <>
+    <Head>
+      <title>CoursesBox</title>
+      <meta name="description" content="IT courses for everyone" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <CoursesWrapper>
+      {courses.map(
+        ({
+          id,
+          attributes: {
+            header,
+            subtitle,
+            publishedAt,
+            cover: {
+              data: {
+                attributes: {
+                  formats: {
+                    medium: { url, width, height },
+                  },
+                },
+              },
+            },
+          },
+        }) => (
+          <Course
+            key={id}
+            header={header}
+            link={`/course/${id}`}
+            imageProps={{
+              width,
+              height,
+              alt: `Cover for ${header}`,
+              src: `${strapi_url}${url}`,
+            }}
+          >
+            <h3>{subtitle}</h3>
+            <time dateTime={publishedAt}>
+              {new Date(publishedAt).toDateString()}
+            </time>
+          </Course>
+        )
+      )}
+    </CoursesWrapper>
+  </>
+);
 
 export default Home;
