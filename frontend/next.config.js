27a99f1655594fa9899789d00fbfde02b855bafd
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2022-05-12 17:37:50 +0400

    Add domain for images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost:1337", "localhost"],
+    domains: [
+      "localhost:1337",
+      "localhost",
+      "ec2-35-173-183-160.compute-1.amazonaws.com",
+    ],
   },
 };
 
